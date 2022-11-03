5340d3f0b449fea65a558589fd052145d3d529d6
cherryApp <j.cserko@gmail.com>
2022-11-04 00:21:00 +0100

    Hello World

---
+++ 01-hello-world/hello.rs
@@ -0,0 +1,8 @@
+// Source: https://doc.rust-lang.org/stable/rust-by-example/hello.html
+// Compile: rustc hello.rs
+// Run: .\hello
+
+fn main() {
+    println!("Hello World!");
+    println!("I'm a Rustacean!");
+}
